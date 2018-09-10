8c4ad2b7f23fe868a74e98960aa70cc4a53f2ac0
nickmjones <hello@nickmjones.me>
2018-09-10 16:59:09 -0400

    Adds better navigation links behavior

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,5 +4,10 @@ module ApplicationHelper
     @percent_complete = (goal / actual) * 100
     return @percent_complete.to_i
   end
+
+  def current_class?(test_path)
+    return 'active' if request.path == test_path
+    ''
+  end
   
 end
